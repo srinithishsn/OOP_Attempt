95e19e4df85cfedda73d29e38c5c298fa89f890d
srinithish <srinithish@srinithishs-MacBook-Pro.local>
2021-03-11 23:51:47 +0530

    Initial commit

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,21 +1,20 @@
+import processing.core.PApplet;
+
 public class Ball {
-        private int ballHeight;
-        private int ballWidth;
-        public static int diameter = 20;
+    public static final int diameter = 15;
+    private int ballHeight;
+        private int ballPosition;
+        private int speed;
 
-        public int getBallHeight() {
-            return ballHeight;
-        }
-
-        public void setBallHeight(int ballHeight) {
-            this.ballHeight = ballHeight;
-        }
-
-        public int getBallWidth() {
-            return ballWidth;
-        }
-
-        public void setBallWidth(int ballWidth) {
-            this.ballWidth = ballWidth;
-        }
+    public Ball(int ballHeight, int ballPosition, int speed) {
+        this.ballHeight = ballHeight;
+        this.ballPosition = ballPosition;
+        this.speed = speed;
+    }
+    public void move(){
+        ballHeight+=speed;
+    }
+    public void draw(PApplet pApplet) {
+        pApplet.ellipse(ballHeight,ballPosition, diameter, diameter);
+    }
 }
--- src/main/java/FourBalls_OOPAttempt.java
+++ src/main/java/FourBalls_OOPAttempt.java
@@ -1,15 +1,29 @@
 import processing.core.PApplet;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FourBalls_OOPAttempt extends PApplet {
 
     public static final int SCREENWIDTH = 640;
     public static final int SCREENHEIGHT = 480;
-    public static final int TotalBalls = 4;
-    static int Speed=0;
+    public static final int TotalBalls = 5;
+    public static final int BALL4_POSITION = SCREENHEIGHT * 4 / TotalBalls;
+    public static final int BALL3_POSITION = SCREENHEIGHT * 3 / TotalBalls;
+    public static final int BALL2_HEIGHT = SCREENHEIGHT * 2 / TotalBalls;
+    public static final int BALL1_HEIGHT = SCREENHEIGHT * 1 / TotalBalls;
+    public static final int InitialPosition = 0;
 
-    ArrayList<Ball> ballArray = new ArrayList<>();
+    private int Speed1=1;
+    private int Speed2=2;
+    private int Speed3=3;
+    private int Speed4=4;
+
+    Ball ball1;
+    Ball ball2;
+    Ball ball3;
+    Ball ball4;
+    List<Ball> balls;
 
     public static void main(String[] args) {
         PApplet.main("FourBalls_OOPAttempt",args);
@@ -17,27 +31,22 @@ public class FourBalls_OOPAttempt extends PApplet {
 
     @Override
     public void settings() {
-        super.settings();
         size(SCREENWIDTH, SCREENHEIGHT);
     }
 
+
     @Override
     public void setup() {
-        for (int BallNumber = 1; BallNumber <= TotalBalls; BallNumber++) {
-            Ball b = new Ball();
-            b.setBallHeight(SCREENHEIGHT * BallNumber/5);
-            b.setBallWidth(BallNumber);
-            ballArray.add(b);
-        }
-    }
+        ball1 = new Ball(InitialPosition, BALL1_HEIGHT, Speed1);
+        ball2 = new Ball(InitialPosition, BALL2_HEIGHT, Speed2);
+        ball3 = new Ball(InitialPosition, BALL3_POSITION, Speed3);
+        ball4 = new Ball(InitialPosition, BALL4_POSITION, Speed4);
+        balls = Arrays.asList(ball1, ball2, ball3, ball4);
+    };
 
     @Override
     public void draw() {
-        for (Ball b :ballArray) {
-            int ballSpeed = b.getBallWidth() * Speed;
-            int ballHeight = b.getBallHeight();
-            ellipse(ballSpeed, ballHeight,Ball.diameter,Ball.diameter);
-        }
-        Speed++;
+        balls.forEach(Ball::move);
+        balls.forEach(ball -> ball.draw(this));
     }
 }
